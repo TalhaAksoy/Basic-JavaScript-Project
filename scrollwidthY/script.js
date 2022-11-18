0d763aa74e272ac48df83d1afd21caee9a39d009
Talha Aksoy <56833887+TalhaAksoy@users.noreply.github.com>
2022-11-18 23:25:36 +0300

    update max Y scroll height

--- scrollwidthY/script.js
+++ scrollwidthY/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let body = document.querySelector('body');
     var limit = body.offsetHeight - 939;
     let scroll = document.querySelector('div.scroll');
-    var scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight -document.documentElement.clientHeight)
+    var scrollMaxY = document.body.offsetHeight - window.innerHeight;
         window.onscroll = function(){
         var width = (window.scrollY / scrollMaxY) * 100;
         scroll.setAttribute("style",`width:${width}%`);
