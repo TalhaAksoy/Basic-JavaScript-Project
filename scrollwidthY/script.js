631b654ce7bf2a9ecb4fe255d92caf70aeb22390
Talha Aksoy <56833887+TalhaAksoy@users.noreply.github.com>
2022-12-04 21:31:58 +0300

    Update script.js

--- scrollwidthY/script.js
+++ scrollwidthY/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let body = document.querySelector('body');
     var limit = body.offsetHeight - 939;
     let scroll = document.querySelector('div.scroll');
-    var scrollMaxY = document.body.offsetHeight - window.innerHeight;
+    var scrollMaxY = window.scrollY || (document.documentElement.scrollHeight - document.documentElement.clientHeight);
         window.onscroll = function(){
         var width = (window.scrollY / scrollMaxY) * 100;
         scroll.setAttribute("style",`width:${width}%`);
